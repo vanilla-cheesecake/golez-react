4b5e488f91866d6a5efb538780432d7a071a7714
golez <golez.sf@gmail.com>
2023-03-06 12:14:58 +0800

    Adding Styles

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>GOLEZ LLOYD</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Post</a>
+            </div>
+        </nav> 
+);
+}
+
+
+export default Navbar;
